5c6c85e7cadf0c8ecee77d97ac33bbeb418ab51d
Benjamin <benjaminxchhakchhuak@gmail.com>
2023-05-06 15:44:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/userRoutes");
+const messageRoute = require("./routes/messagesRoute");
+
+const app = express();
+const socket = require("socket.io");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./.env" });
+
+app.use(cors());  //{ origin: "https://chat-r-benju2205.netlify.app" }
+app.use(express.json());
+app.use("/api/auth", userRoutes);
+app.use("/api/messages", messageRoute);
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connection Successful");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+const server = app.listen(process.env.PORT, () => {
+  // start the server
+  console.log(`Server Started on Port ${process.env.PORT}`);
+});
+
+
+const io = socket(server, {
+  cors: {
+    origin: process.env.ORIGIN,
+    credentials: true,
+  },
+});
+
+global.onlineUsers = new Map();
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.message);
+    }
+  });
+});
